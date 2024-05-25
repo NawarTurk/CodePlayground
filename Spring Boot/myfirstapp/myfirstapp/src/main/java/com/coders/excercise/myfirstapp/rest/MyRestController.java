3044dc45ce66a9bc8c1c50f8344ad378683546d5
Nawar T <nawarturk11@gmail.com>
2024-05-25 14:44:24 -0400

    Add Spring Boot DevTools dependency to pom.xml

---
+++ Spring Boot/myfirstapp/myfirstapp/src/main/java/com/coders/excercise/myfirstapp/rest/MyRestController.java
@@ -0,0 +1,29 @@
+package com.coders.excercise.myfirstapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyRestController {
+
+    // expose "/" endpoint that returns "Hello World!"
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World!";
+    }
+
+    // expose a new endpoint
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k!|";
+    }
+
+
+    @GetMapping("/fortune")
+    public String getDailyfortune() {
+        System.out.println("hello");
+        return "today is your lucky day!";
+    }
+}
