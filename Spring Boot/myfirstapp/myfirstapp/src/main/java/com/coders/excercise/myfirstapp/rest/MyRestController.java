ad53850c134433db3286aefd771f1adea7ac3a56
Nawar T <nawarturk11@gmail.com>
2024-05-27 15:12:47 -0400

    feat: Add and inject custom properties, and creat /teaminfo endpoint

--- Spring Boot/myfirstapp/myfirstapp/src/main/java/com/coders/excercise/myfirstapp/rest/MyRestController.java
+++ Spring Boot/myfirstapp/myfirstapp/src/main/java/com/coders/excercise/myfirstapp/rest/MyRestController.java
@@ -1,29 +1,39 @@
 package com.coders.excercise.myfirstapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyRestController {
 
-    // expose "/" endpoint that returns "Hello World!"
+    // inject properties
+    @Value("${coach.name}")
+    private String coachName;
 
+    @Value("${team.name}")
+    private String teamName;
+
+    // expose "/" endpoint that returns "Hello World!"
     @GetMapping("/")
     public String sayHello() {
         return "Hello World!";
     }
 
     // expose a new endpoint
-
     @GetMapping("/workout")
     public String getDailyWorkout() {
         return "Run a hard 5k!|";
     }
 
-
     @GetMapping("/fortune")
     public String getDailyfortune() {
         System.out.println("hello");
         return "today is your lucky day!";
+    }
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + ", Team name: " + teamName;
     }
 }
