7df454b72a7ea1096fe004fde498ec994cb2214c
Nawar T <nawarturk11@gmail.com>
2024-06-10 23:37:35 -0400

    feat: Add an exception handler using @ExceptionHandler

--- Spring Boot/restcruddemo/src/main/java/com/nawarapps/restcruddemo/rest/StudentRestController.java
+++ Spring Boot/restcruddemo/src/main/java/com/nawarapps/restcruddemo/rest/StudentRestController.java
@@ -2,10 +2,9 @@ package com.nawarapps.restcruddemo.rest;
 
 import com.nawarapps.restcruddemo.entity.Student;
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,9 +34,19 @@ public class StudentRestController {
         if (studentId < 0 || studentId >= studentList.size()) {
             throw new StudentNotFoundException("Student Id not found - " + studentId);
         }
-        
+
         return studentList.get(studentId);
     }
 
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
+        StudentErrorResponse error = new StudentErrorResponse();
 
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        // To do: Check this
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
 }
