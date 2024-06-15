1407cffac76db4e18878df2d535e61e01bddf686
nawarturk <nawarturk11@gmail.com>
2024-06-15 12:55:23 -0400

    feat: Add generic exception handler

--- Spring Boot/restcruddemo/src/main/java/com/nawarapps/restcruddemo/rest/StudentRestController.java
+++ Spring Boot/restcruddemo/src/main/java/com/nawarapps/restcruddemo/rest/StudentRestController.java
@@ -49,4 +49,15 @@ public class StudentRestController {
         // To do: Check this
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exc.getMessage());  // we could customize this error msg
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
 }
