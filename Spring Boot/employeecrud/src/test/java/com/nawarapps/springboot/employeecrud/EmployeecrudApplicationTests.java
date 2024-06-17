412d8e780f3e2461defebb6874967dc911c47d2d
Nawar T <nawarturk11@gmail.com>
2024-06-16 23:46:32 -0400

    add: New Spring Boot project 'employeecrud'

---
+++ Spring Boot/employeecrud/src/test/java/com/nawarapps/springboot/employeecrud/EmployeecrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nawarapps.springboot.employeecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeecrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
