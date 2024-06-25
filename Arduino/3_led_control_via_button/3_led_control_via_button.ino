72fa6f4ab99a2c01847b0c566ca6e9e5304c4b5c
Nawar T <nawarturk11@gmail.com>
2024-06-25 10:09:20 -0400

    arduino/add: Control LED via bush button

---
+++ Arduino/3_led_control_via_button/3_led_control_via_button.ino
@@ -0,0 +1,15 @@
+#define LED_PIN 10
+#define BUTTON_PIN 2
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT);
+}
+
+void loop() {
+  if (digitalRead(BUTTON_PIN) == HIGH) {
+    digitalWrite(LED_PIN, HIGH);
+  } else {
+    digitalWrite(LED_PIN, LOW);
+  }
+}
