9dd55a848bb0aa71c610f8bb35547960e77f3e05
nawarturk <nawarturk11@gmail.com>
2024-07-22 23:49:46 -0400

    refactor(react test): replace fireEvent with userEvent and update @testing-library/user-event to v14.5.2

--- React testing/02_icecream_shop_test/src/pages/summary/tests/SummaryForm.test.jsx
+++ React testing/02_icecream_shop_test/src/pages/summary/tests/SummaryForm.test.jsx
@@ -1,30 +1,35 @@
+import userEvent from "@testing-library/user-event";
 import SummaryForm from "../SummaryForm";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 test("Initial conditions", () => {
   render(<SummaryForm />);
   const checkboxElement = screen.getByRole("checkbox", {
     name: /terms and conditions/i,
   });
-  const buttonElement = screen.getByRole("button", { name: /confirm order/i });
+  const confirmButton = screen.getByRole("button", { name: /confirm order/i });
 
   // check initial state
   expect(checkboxElement).not.toBeChecked();
-  expect(buttonElement).toBeDisabled();
+  expect(confirmButton).toBeDisabled();
 });
 
-test("Checkbox enables button on first click and disables on second click", () => {
+test("Checkbox enables button on first click and disables on second click", async () => {
+  const user = userEvent.setup();
+
   render(<SummaryForm />);
   const checkboxElement = screen.getByRole("checkbox", {
     name: /terms and conditions/i,
   });
-  const buttonElement = screen.getByRole("button", { name: /confirm order/i });
+  const confirmButton = screen.getByRole("button", { name: /confirm order/i });
 
   // click the checkbox to enable the button
-  fireEvent.click(checkboxElement);
-  expect(buttonElement).toBeEnabled();
+  await user.click(checkboxElement);
+  expect(confirmButton).toBeEnabled();
 
   // click the checkbox to disable the button
-  fireEvent.click(checkboxElement);
-  expect(buttonElement).toBeDisabled();
+  await user.click(checkboxElement);
+  expect(confirmButton).toBeDisabled();
 });
+
+// npm install @testing-library/user-event@latest
