f3f61bc3634e5aef192733dab8dd034967e8ac88
Nawar T <nawarturk11@gmail.com>
2024-08-18 10:49:24 -0400

    add(flutter): add icon and transparency to quiz app start screen

--- Flutter/flutter_02_quiz_app/lib/home_page.dart
+++ Flutter/flutter_02_quiz_app/lib/home_page.dart
@@ -9,29 +9,45 @@ class HomePage extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Image.asset(
-            'assets/images/quiz-logo.png',
-            width: 250,
+          Opacity(
+            opacity: 0.7,
+            child: Image.asset(
+              'assets/images/quiz-logo.png',
+              width: 250,
+              color: const Color.fromARGB(100, 255, 255, 255),
+            ),
           ),
+          // Opacity(  // Opactiy is performance intensive, try avoid to use it
+          //   opacity: 0.7,
+          //   child: Image.asset(
+          //     'assets/images/quiz-logo.png',
+          //     width: 250,
+          //   ),
+          // ),
           const SizedBox(
             height: 50,
           ),
           const Text(
             'Learn Flutter!',
             style: TextStyle(
-              color: Colors.greenAccent,
+              color: Color.fromARGB(255, 194, 231, 212),
               fontSize: 27,
             ),
           ),
           const SizedBox(
             height: 20,
           ),
-          TextButton(
+          OutlinedButton.icon(
             onPressed: () {},
             style: OutlinedButton.styleFrom(
               foregroundColor: Colors.white,
             ),
-            child: const Text('Press Me!'),
+            // child: const Text('Press Me!'), it is now called label
+            label: const Text('Press Me!'),
+            icon: const Icon(
+              Icons // can be used anywhere that requiresa a widget
+                  .arrow_right_alt,
+            ),
           ),
         ],
       ),
