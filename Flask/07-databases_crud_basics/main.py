788452ccbad93c584c5cb6b814069979c5c569b6
nawarturk <nawarturk11@gmail.com>
2024-09-08 21:02:52 -0400

    add(flask): Flask migration

--- Flask/07-databases_crud_basics/main.py
+++ Flask/07-databases_crud_basics/main.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate # pip install Flask-Migrate
 
 # 1. Create an SQLite database purely via Python
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -21,8 +22,10 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable modification tra
 
 db = SQLAlchemy(app)  # Instantiate SQLAlchemy with the Flask app to create the database connection
 
-# 2. Creating a model 
+Migrate(app, db) # connect flask app with the db in order to add on migration capabilities
 
+
+# 2. Creating a model 
 class Puppy(db.Model):
     # When passing the app through SQLAlchemy, db will have a model class to represent tables in the database
     # Default table name is typically the lowercase version of the class name; to override:
@@ -32,11 +35,35 @@ class Puppy(db.Model):
     id = db.Column(db.Integer, primary_key=True)  # A unique identifier for each row
     name = db.Column(db.Text)  # Column for the puppy's name
     age = db.Column(db.Integer)  # Column for the puppy's age
+    breed = db.column(db.Text)
 
-    def __init__(self, name, age):  # Constructor doesn't require id as it's automatically generated
+    def __init__(self, name, age, breed):  # Constructor doesn't require id as it's automatically generated
         self.name = name
         self.age = age
+        self.breed = breed
     
     def __repr__(self):  # String representation of the object, helpful for debugging
         return f"Puppy {self.name} is {self.age} year(s) old"
+
+# Set the FLASK_APP environment variable to 'main.py'.
+# This tells Flask which file to use as the entry point of the application.
+# $env:FLASK_APP="main.py"
+
+# Verify that the FLASK_APP environment variable is set correctly.
+# This command prints the value of FLASK_APP to ensure it points to your main application file.
+# echo $env:FLASK_APP
+
+# Initialize the migration environment.
+# This command sets up the migrations directory, where all migration scripts will be stored.
+# It only needs to be run once at the beginning to prepare the project for handling migrations.
+# flask db init
+
+# Generate a migration script automatically.
+# This should be done whenever you make changes to your database models (e.g., adding a new table or field).
+# flask db migrate -m "created puppy table"
+
+# Apply the migration to the database.
+# This command updates the database schema to match the current state of your models as described in the migration scripts.
+# Run this after generating a new migration script to make the actual schema changes to the database.
+# flask db upgrade
 
